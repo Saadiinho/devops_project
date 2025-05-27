343bbc5d70780415a702c716c988425f5176a84b
Saad R <saadrafiqul93380@gmail.com>
2025-05-27 19:07:14 +0200

    Add database in healthcheck

--- devops_project/main.py
+++ devops_project/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import os
+import mysql.connector
+from mysql.connector import Error
 
 app = FastAPI()
 
@@ -16,6 +18,31 @@ MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
 DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
 
+
+def test_db_connection() -> str:
+    try:
+        connection = mysql.connector.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DATABASE
+        )
+        if connection.is_connected():
+            return "Successful connexion"
+    except Error as e:
+        return "Unsuccessful connection"
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
+
+
 @app.get("/healthcheck")
 def root():
-    return {"name": "devops_project API", "version": __version__, "status": "running"}
+    return {
+        "name": "devops_project API", 
+        "version": __version__, 
+        "status": {
+            "application": "running",
+            "database": test_db_connection()
+        }
+    }
