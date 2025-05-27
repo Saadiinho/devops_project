18b7b4117dfec21a8b9f96112bec9ce40d8df5d5
Saad R <saadrafiqul93380@gmail.com>
2025-05-27 18:55:17 +0200

    Add endpoint healthcheck

--- devops_project/main.py
+++ devops_project/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
-
-import os
+from fastapi import FastAPI
 
 app = FastAPI()
+
+__version__ = "1.0.0"
+
+
+
 
 # Lire les variables d'environnement
 MYSQL_USER = os.getenv("MYSQL_USER")
@@ -15,26 +16,7 @@ MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
 
 DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
-# Exemple d'un modèle simple
-class DomainSkills(Base):
-    __tablename__ = "domain_skills"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(length=100))
-
-# Dependency pour récupérer la session DB
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.get("/")
-def read_root(db: Session = Depends(get_db)):
-    domain_skills = db.query(DomainSkills).all()
-    return {"domain skills": [domain_skill.name for domain_skill in domain_skills]}
+@app.get("/healthcheck")
+def root():
+    return {"name": "devops_project API", "version": __version__, "status": "running"}
