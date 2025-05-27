0add2fa58b661d34e138939054dcbaa500f51108
Saad R <saadrafiqul93380@gmail.com>
2025-05-27 19:13:27 +0200

    Bandit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from devops_project.main import app
 from devops_project.main import __version__
+
 client = TestClient(app)
 
 
@@ -10,8 +11,5 @@ def test_read_root():
     assert response.json() == {
         "name": "devops_project API",
         "version": __version__,
-        "status": {
-            "application": "running", 
-            'database': 'Unsuccessful connection'
-        },
+        "status": {"application": "running", "database": "Unsuccessful connection"},
     }
